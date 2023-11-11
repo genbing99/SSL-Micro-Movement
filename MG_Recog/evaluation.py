be8d5e4324e590b301f8c17c3266140368de7a2d
Gen Bing <genbing67@gmail.com>
2023-11-11 17:30:09 +0800

    Initial commit

--- MG_Recog/evaluation.py
+++
@@ -1,9 +0,0 @@
-import torch
-    
-def topKCorrect(gt, yhat, k):
-    values, indices = torch.topk(yhat, k)
-    correct = 0
-    for index in range(len(gt)):
-        if gt[index] in indices[index]:
-            correct += 1
-    return correct
